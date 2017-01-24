7b2058a5f7f4423768fc9933582c425437b32c49
Davide Ramo <davide.ramo@finantix.com>
2017-01-24 13:33:04 +0100

    Added presentation style in iOS auth config Used presentation style in iOS authorizer

--- Sources/Base/OAuth2AuthConfig.swift
+++ Sources/Base/OAuth2AuthConfig.swift
@@ -35,6 +35,10 @@ public struct OAuth2AuthConfig {
 		
 		/// Starting with iOS 9, `SFSafariViewController` will be used for embedded authorization instead of our custom class. You can turn this off here.
 		public var useSafariView = true
+
+        /// By assigning your own UIModalPresentationStyle (!) you can configure how the embedded authorisation is presented.
+        public var modalPresentationStyle = 0
+
 	}
 	
 	/// Whether the receiver should use the request body instead of the Authorization header for the client secret; defaults to `false`.
